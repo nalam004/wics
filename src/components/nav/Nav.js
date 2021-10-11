aca33c30e52d8e9ea48be794676f26abbb6b7582
PETERYE100 <pye000@citymail.cuny.edu>
2021-10-11 00:57:59 -0400

    navbar styles added

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import './Nav.css'; 
 import logo from '../../assets/logo.svg'
 
@@ -6,10 +6,31 @@ function Nav() {
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
- 
+    
+    let listener = null
+    const [scrollState, setScrollState] = useState("top")
+
+    useEffect(() => {
+        listener = document.addEventListener("scroll", e => {
+        var scrolled = document.scrollingElement.scrollTop
+        if (scrolled >= 120) {
+            if (scrollState !== "not_top") {
+            setScrollState("not_top")
+            }
+        } else {
+            if (scrollState !== "top") {
+            setScrollState("top")
+            }
+        }
+        })
+        return () => {
+        document.removeEventListener("scroll", listener)
+        }
+    }, [scrollState])
+
     return (
     <>
-            <nav className='navbar'>
+            <nav className='navbar' style={{background: scrollState === "top" ? "linear-gradient(180deg, rgba(175, 177, 232, 0.864583) 12.5%, rgba(175, 177, 232, 0.717604) 41.15%, rgba(175, 177, 232, 0.406354) 78.65%, rgba(30, 33, 38, 0) 100%)" : "rgb(175, 177, 232)"}}>
                   <div className='nav-container'>
                             <img width='90px' src={logo} alt=''/>
                             <div className='menu-icon' onClick={handleClick}>
@@ -18,16 +39,16 @@ function Nav() {
                             
                             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                                 <li className='nav-item'>   
-                                    <a className='nav-link' onClick={closeMobileMenu} href="#about">About</a>
+                                    <a className='nav-link from-center' onClick={closeMobileMenu} href="#about">About</a>
                                 </li>
                                 <li className='nav-item'>
-                                    <a className='nav-link' onClick={closeMobileMenu} href="#events">Events</a>    
+                                    <a className='nav-link from-center' onClick={closeMobileMenu} href="#events">Events</a>    
                                 </li> 
                                 <li className='nav-item'>
-                                    <a className='nav-link' onClick={closeMobileMenu} href="#members">Members</a>
+                                    <a className='nav-link from-center' onClick={closeMobileMenu} href="#members">Members</a>
                                 </li>
                                 <li className='nav-item'>
-                                    <a className='nav-link' onClick={closeMobileMenu} href="#collabs">Collaborators</a>
+                                    <a className='nav-link from-center' onClick={closeMobileMenu} href="#collabs">Collaborators</a>
                                 </li>
                             </ul>    
                         </div>
